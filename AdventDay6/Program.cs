39f89e2d61f533f6af070da68bb9434ea8c4914b
Isaiah Banks <isaiah.banks42@gmail.com>
2022-12-06 18:29:04 -0500

    Day 6. This is one was pretty easy :)

---
+++ AdventDay6/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventDay6;
+
+internal class Program
+{
+    private static void Main()
+    {
+        string input = GetInput();
+        CommunicationChecker9001(input, 4);
+        CommunicationChecker9001(input, 14);
+    }
+
+    private static string GetInput()
+    {
+        return new StreamReader(new FileStream("input.txt", FileMode.Open, FileAccess.Read)).ReadToEnd();
+    }
+
+    private static void CommunicationChecker9001(string input, int uniqueCharacters)
+    {
+        var characters = string.Empty;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (characters.Length == uniqueCharacters)
+                characters = characters.Remove(0, 1);
+
+            characters += input[i];
+
+            if (characters.Length == uniqueCharacters && !ContainsDuplicates(characters))
+            {
+                Console.WriteLine($"First marker after: {i + 1}");
+                break;
+            }
+        }
+    }
+
+    private static bool ContainsDuplicates(string input)
+    {
+        return input.GroupBy(c => c).Any(g => g.Count() > 1);
+    }
+}
